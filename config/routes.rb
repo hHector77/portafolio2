450834ef01073b043fdd7aee16657a32e5078fae
hhector77 <orellahhector@gmail.com>
2024-09-30 23:35:55 -0300

    Generado PagesController con acciones home, projects y contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "pages/home"
+  get "pages/projects"
+  get "pages/contact"
   root 'pages#home'      # Define la raíz de la aplicación como /home
   get 'projects', to: 'pages#projects'
   get 'contact', to: 'pages#contact'
